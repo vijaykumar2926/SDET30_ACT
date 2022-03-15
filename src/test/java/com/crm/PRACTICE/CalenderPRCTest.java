514746d9b5e693ef17e69744d0dca8e4d8b1cb33
vijay <vijay@LAPTOP-0SCJ6J7A>
2022-03-15 16:49:43 +0530

    xgchashscbkj

--- src/test/java/com/crm/PRACTICE/CalenderPRCTest.java
+++ src/test/java/com/crm/PRACTICE/CalenderPRCTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class CalenderPRC 
+public class CalenderPRCTest 
 {
 	@Test
 	public void Selectdate() throws Throwable
