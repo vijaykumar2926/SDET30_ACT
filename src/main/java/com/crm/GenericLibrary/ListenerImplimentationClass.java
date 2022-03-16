75877c0c71fac6add8725460cf2c31f2c72639c9
vijay <vijay@LAPTOP-0SCJ6J7A>
2022-03-16 16:37:53 +0530

    listener

--- src/main/java/com/crm/GenericLibrary/ListenerImplimentationClass.java
+++ src/main/java/com/crm/GenericLibrary/ListenerImplimentationClass.java
@@ -4,16 +4,42 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class ListenerImplimentationClass implements ITestListener
 {
 
+	ExtentReports report;
+	ExtentTest test;
+	
+	
 	public void onFinish(ITestContext context) 
 	{
-		
+		// consolidate all the parameter and generate report
+		report.flush();
 	}
 
 	public void onStart(ITestContext context) {
 
+		//Execution starts here 
+		/* configure the report */
+		ExtentSparkReporter htmlreport = new ExtentSparkReporter("./ExtentsReport"+new JavaUtility().getsystemdateinformat());
+		htmlreport.config().setDocumentTitle("SDET-30 Execution Report");
+		htmlreport.config().setTheme(Theme.STANDARD);
+		htmlreport.config().setReportName("Selenium Execution Report");
+		
+		report = new ExtentReports();
+		report.attachReporter(htmlreport);
+		report.setSystemInfo("Base-Browser", "chrome");
+		report.setSystemInfo("OS", "windows");
+		report.setSystemInfo("Base-url", "http://localhost:8888");
+		report.setSystemInfo("Reporter name", "vijay");
+
+		
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
@@ -42,12 +68,18 @@ public class ListenerImplimentationClass implements ITestListener
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
+		
+		test.log(Status.FAIL, MethodName+"---> TestScript FAILED");
+
 	}
 
 	public void onTestSkipped(ITestResult result) {
 	
 		String MethodName =	result.getMethod().getMethodName();
-		System.out.println(MethodName+"---> TestScript SKIPPED");
+		test.log(Status.SKIP, MethodName+"---> TestScript SKIPPED");
+		// This will capture the exception and log it in report
+		test.log(Status.SKIP, result.getThrowable());
+
 	}
 
 	public void onTestStart(ITestResult result) {
